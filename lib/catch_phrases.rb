e16f903df5dad39a112cbbf52992b3d8705bb331
Jeremy Winn <jeremyjwinn@gmail.com>
2020-07-08 21:51:35 +0000

    Automatically backed up by Learn

--- lib/catch_phrases.rb
+++ lib/catch_phrases.rb
@@ -7,13 +7,13 @@ end
 def toadstool
   status = 'Thank You Mario! But Our Princess Is In Another Castle!'
   puts status
-  status
+  
 end
 
 def link
   phrase = "It's Dangerous To Go Alone! Take This."
   puts phrase
-  phrase
+  
 end
 
 def all_phrases
